823b19c9d86024378f533a4eb707522d7fd7dc99
Esther White <monacodelisa@gmail.com>
2024-06-02 11:14:26 +0300

    isPlatformBrowser check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, inject, OnInit, PLATFORM_ID } from "@angular/core";
+import { CommonModule, DOCUMENT, isPlatformBrowser } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = 'angular-ssr';
+export class AppComponent implements OnInit {
+	title = "angular-ssr";
+	doc = inject(DOCUMENT);
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+  }
+
+	ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      console.log("Browser");
+    }
+	}
 }
